d8ac5ecc2f665c97d65df50bc6d536437c39f28b
Santiago Cuello <sjcuello@gmail.com>
2024-09-14 20:08:00 -0300

    Update basic slice

--- src/redux/items/slice.ts
+++ src/redux/items/slice.ts
@@ -28,6 +28,35 @@ export const itemsSlice = createSlice({
     setDeletedList: (state, action) => {
       state.deletedList = action.payload;
     },
+    updateItem: (state, action) => { 
+      const itemToUpdate = state.itemList.find((item) => item.id === action.payload.id);
+      if (itemToUpdate) {
+        itemToUpdate.name = action.payload.name;
+        itemToUpdate.description = action.payload.description;
+        itemToUpdate.amount = action.payload.amount;
+        itemToUpdate.isChecked = action.payload.isChecked;
+      }
+    },
+    deleteItem: (state, action) => { 
+      const itemToDelete = state.itemList.find((item) => item.id === action.payload);
+      if (itemToDelete) {
+        state.deletedList.push(itemToDelete);
+        state.itemList = state.itemList.filter((item) => item.id !== action.payload);
+      }
+    },
+    restoreItem: (state, action) => { 
+      const itemToRestore = state.deletedList.find((item) => item.id === action.payload);
+      if (itemToRestore) {
+        state.itemList.push(itemToRestore);
+        state.deletedList = state.deletedList.filter((item) => item.id !== action.payload);
+      }
+    },
+    checkItem: (state, action) => { 
+      const itemToCheck = state.itemList.find((item) => item.id === action.payload);
+      if (itemToCheck) {
+        itemToCheck.isChecked = !itemToCheck.isChecked;
+      }
+    }
   },
 });
 
@@ -40,20 +69,6 @@ export const selectItemList = (state: RootState) => state.items.itemList;
 export const selectDeletedList = (state: RootState) => state.items.deletedList;
 export const selectItemById = (state: RootState, id: number) => state.items.itemList.find((item) => item.id === id);
 export const selectDeletedItemById = (state: RootState, id: number) => state.items.deletedList.find((item) => item.id === id);
-export const deleteItemById = (state: RootState, id: number) => { 
-  const itemToDelete = state.items.itemList.find((item) => item.id === id);
-  if (itemToDelete) {
-    state.items.deletedList.push(itemToDelete);
-    state.items.itemList = state.items.itemList.filter((item) => item.id !== id);
-  }
-}
-export const restoreItemById = (state: RootState, id: number) => { 
-  const itemToRestore = state.items.deletedList.find((item) => item.id === id);
-  if (itemToRestore) {
-    state.items.itemList.push(itemToRestore);
-    state.items.deletedList = state.items.deletedList.filter((item) => item.id !== id);
-  }
-}
 
 
 
