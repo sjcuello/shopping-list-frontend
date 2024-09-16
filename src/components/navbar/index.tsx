09fd10f6fee1e89c51d2ab4488b9ffafae8dccfd
Santiago Cuello <sjcuello@gmail.com>
2024-09-16 02:07:55 -0300

    First Trash bin

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,16 +1,25 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
-import { Add as AddIcon } from '@mui/icons-material';
-// import { useState } from 'react';
+import { AppBar, Box, IconButton, Toolbar, Tooltip, Typography } from '@mui/material';
+import { Add as AddIcon, DeleteSweepOutlined as DeleteSweepOutlinedIcon } from '@mui/icons-material';
 import styles from './styles.module.css';
 import Drawer from '../drawer';
 import { useAppDispatch } from '../../redux';
 import { switchDrawer } from '../../redux/drawer';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   const dispatch = useAppDispatch();
   const handleDrawerToggle = () => {
     dispatch(switchDrawer());
   };
+  const navigate = useNavigate();
+
+  const handleTrashClick = () => {
+    navigate('/trash-bin');
+  };
+
+  const handleHomeClick = () => {
+    navigate('/');
+  }
 
   return (
     <Box className={styles.headerContainer}>
@@ -20,18 +29,34 @@ const Navbar: React.FC = () => {
             variant="h6"
             component="div"
             className={styles.title}
+            onClick={handleHomeClick}
           >
             Shopping List
           </Typography>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={styles.menuIcon}
-          >
-            <AddIcon />
-          </IconButton>
+          <Box className={styles.iconContainer}>
+            <IconButton
+              color="inherit"
+              aria-label="open trash bin"
+              edge="start"
+              onClick={handleTrashClick}
+              className={styles.icon}
+            >
+              <Tooltip title="Trash bin">
+                <DeleteSweepOutlinedIcon />
+              </Tooltip>
+            </IconButton>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              className={styles.icon}
+            >
+              <Tooltip title="Add Item">
+                <AddIcon />
+              </Tooltip>
+            </IconButton>
+          </Box>
         </Toolbar>
       </AppBar>
       <Drawer />
