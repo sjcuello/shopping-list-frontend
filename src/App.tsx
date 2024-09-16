52a5b96235732676e492a2a91f81ce74a91b2913
Santiago Cuello <sjcuello@gmail.com>
2024-09-16 01:42:09 -0300

    First router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import CardsContainer from './components/cardsContainer'
+import ErrorPage from './components/errorPage';
 import Layout from './components/layout'
 import Navbar from './components/navbar'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CardsContainer />,
+  },
+  {
+    path: "/trash-bin",
+    element: <div>trash-bin</div>,
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  }
+]);
+
 
 function App() {
   return (
     <Layout>
       <Navbar />
-      <CardsContainer />
+      <RouterProvider router={router} />
     </Layout>
   )
 }
